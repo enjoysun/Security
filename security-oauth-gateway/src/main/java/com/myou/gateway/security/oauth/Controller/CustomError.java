0148205046a81cad0048eb10c0d2407e107a22b3
youming <1570819807@qq.com>
2019-10-16 15:43:25 +0800

    提交图片

---
+++ security-oauth-gateway/src/main/java/com/myou/gateway/security/oauth/Controller/CustomError.java
@@ -0,0 +1,11 @@
+package com.myou.gateway.security.oauth.Controller;
+
+/*
+ * @Time    : 2019/10/16 11:37 AM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : CustomError.java
+ * @Software: IntelliJ IDEA
+ */
+public class CustomError {
+}
---
+++ spring-cloud-service/src/main/java/com/myou/spring/cloud/service/server/WebSecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.myou.spring.cloud.service.server;
+
+/*
+ * @Time    : 2019/10/16 10:34 AM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : WebSecurityConfiguration.java
+ * @Software: IntelliJ IDEA
+ */
+public class WebSecurityConfiguration {
+}
