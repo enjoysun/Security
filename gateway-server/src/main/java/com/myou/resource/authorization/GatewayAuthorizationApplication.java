d6219ded674a8611cdae405d08874d27d3c3a2da
youming <1570819807@qq.com>
2019-10-21 16:35:12 +0800

    项目结构调整

---
+++ gateway-server/src/main/java/com/myou/resource/authorization/GatewayAuthorizationApplication.java
@@ -0,0 +1,18 @@
+package com.myou.resource.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * @Time    : 2019/10/21 4:33 PM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : GatewayAuthorizationApplication.java
+ * @Software: IntelliJ IDEA
+ */
+@SpringBootApplication
+public class GatewayAuthorizationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayAuthorizationApplication.class, args);
+    }
+}
