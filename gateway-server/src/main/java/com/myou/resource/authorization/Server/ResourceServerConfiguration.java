358efe0f351206c6e7447cef30731884ee071eb2
youming <1570819807@qq.com>
2019-10-22 16:38:34 +0800

    认证授权服务和资源服务分离设计完整结果一次流程

--- gateway-server/src/main/java/com/myou/resource/authorization/Server/ResourceServerConfiguration.java
+++ gateway-server/src/main/java/com/myou/resource/authorization/Server/ResourceServerConfiguration.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.authentication.TokenExtractor;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,36 +24,23 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-    @Autowired
-    private TokenStore tokenStore;
 
-    @Bean
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore);
-        return defaultTokenServices;
-    }
-
-    public ResourceServerConfiguration() {
-        super();
-    }
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.tokenExtractor(new TokenExtractor() {
-            @Override
-            public Authentication extract(HttpServletRequest request) {
-                // 提取请求中的令牌
-                return null;
-            }
-        });
-        resources.tokenStore(tokenStore);
-    }
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//        resources.tokenExtractor(new TokenExtractor() {
+//            @Override
+//            public Authentication extract(HttpServletRequest request) {
+//                // 提取请求中的令牌
+//                return null;
+//            }
+//        });
+//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/test").hasRole("ADMIN");
+                .antMatchers(HttpMethod.GET, "/test").hasAuthority("ROLE_ADMIN");
     }
 }
