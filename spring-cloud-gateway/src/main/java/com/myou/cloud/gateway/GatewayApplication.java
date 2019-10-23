2698a7ded312b9f177b068d1db4f54ef761e799c
youming <1570819807@qq.com>
2019-10-23 16:39:57 +0800

    完善授权服务

---
+++ spring-cloud-gateway/src/main/java/com/myou/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,11 @@
+package com.myou.cloud.gateway;
+
+/*
+ * @Time    : 2019/10/23 4:31 PM
+ * @Author  : YouMing
+ * @Email   : myoueva@gmail.com
+ * @File    : GatewayApplication.java
+ * @Software: IntelliJ IDEA
+ */
+public class GatewayApplication {
+}
